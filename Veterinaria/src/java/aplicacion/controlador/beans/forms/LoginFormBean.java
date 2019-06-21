d943bf19203f9f54ebd9c0c5ba4934abe1826622
mabel <>
2019-06-21 19:00:22 -0300

    modificando el index y la clase login

--- Veterinaria/src/java/aplicacion/controlador/beans/forms/LoginFormBean.java
+++ Veterinaria/src/java/aplicacion/controlador/beans/forms/LoginFormBean.java
@@ -73,7 +73,8 @@ private String nombreUsuario;
      */
     public String getNombreUsuarioValidado() {
         Usuario usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioValidado");
-        return usuario.getNombreUsuario();
+        //return usuario.getNombreUsuario();
+        return usuario.getNombre();
     }
     
     /**
